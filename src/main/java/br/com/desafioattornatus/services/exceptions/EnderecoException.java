4bcde6ea76d31a1908bb1c3e155cde8e9f46e6b3
Thales Jose <thalesjoseaguiar@gmail.com>
2023-04-12 14:51:25 -0300

    exclusão de metodos não utilizados

--- src/main/java/br/com/desafioattornatus/services/exceptions/EnderecoException.java
+++ src/main/java/br/com/desafioattornatus/services/exceptions/EnderecoException.java
@@ -4,11 +4,6 @@ public class EnderecoException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public EnderecoException(String message, Throwable cause) {
-		super(message, cause);
-		
-	}
-
 	public EnderecoException(String message) {
 		super(message);
 		
